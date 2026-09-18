7a6c20f416f9853b51d5f2ef7cbb88a8c27cd201
MaseyCodey <hellerud.mason@gmail.com>
2026-09-18 10:15:41 -0700

    Add configurable branding and logo

---
+++ public/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Change these values to rebrand the entire website.
+window.APP_CONFIG=Object.freeze({
+  siteName:"Mase's Music",
+  logoUrl:'/assets/music-logo.png',
+  logoLink:'/'
+});
